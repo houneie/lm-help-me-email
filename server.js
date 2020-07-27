faa7ff89555ca9044332b1b9640d706cea502556
Abdul Rehman <houneie@gmail.com>
2020-07-27 12:12:05 +0500

    Everything is working fien

---
+++ server.js
@@ -0,0 +1,42 @@
+"use strict";
+const nodemailer = require("nodemailer");
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("<h>Welcome to LM Help Emailer</h>")
+  })
+  
+app.get('/helpMe', (req, res) => {
+    var toMail = req.query.toMail
+    var from = req.query.from
+    var location = req.query.loc
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'lm.help.me20@gmail.com',
+          pass: 'Z535HrHejh9oJe'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'lm.help.me20@gmail.com',
+        to: toMail,
+        subject: from +' needs your help',
+        text: from + ' Location is ' + location
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    res.send("Sent")
+  })
+
+app.listen(80)
+
